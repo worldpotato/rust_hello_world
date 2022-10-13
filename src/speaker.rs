b000317aade6453e34344d16fa157551cecf7ca7
Felix Strobel <mail@felixstrobel.de>
2022-10-13 17:08:23 +0200

    fix warning that mut is not needed

--- src/speaker.rs
+++ src/speaker.rs
@@ -16,7 +16,7 @@ pub fn my_scream(message: &String) -> i32 {
 
 pub fn my_whisper(message: &String) -> i32 {
     /*! Print the given string as a speack bubble of a crab */
-    let mut lower = message.to_lowercase();
+    let lower = message.to_lowercase();
 
     let stdout = stdout();
     let width = message.chars().count();
